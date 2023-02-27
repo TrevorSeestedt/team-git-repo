efb13c0eb1f8c57b21decb4d77964ae831391c9b
Griffin Miller <toezmills@gmail.com>
2023-02-26 20:11:45 -0500

    testing commit with 30 seconds method

--- command/InputHandler.java
+++ command/InputHandler.java
@@ -1,15 +1,7 @@
 import java.util.HashMap;
 public class InputHandler {
     private HashMap<String, Command> commands = new HashMap<String, Command>();
-<<<<<<< HEAD
-
-    public InputHandler(StopWatch watch) {
-
-    }
-    public boolean inputEntered(String data) {
-=======
     private StopWatch watch;
-
     /**
      * Function will allow the program to take in the inputs by the user 
      * @author Trevor Seestedt
@@ -21,7 +13,7 @@ public class InputHandler {
         commands.put("30", new ThirtySecCommand(watch));
     }
     /**
-     * Functio checks the inputs that the user enters
+     * Function checks the inputs that the user enters
      * @author Trevor Seestedt
      */
     public boolean inputEntered(String data) {
@@ -29,7 +21,6 @@ public class InputHandler {
         if (command == null)
             return false; 
         command.execute();
->>>>>>> 59ffe647dd5ba71991adf97b21240436f10d16f5
         return true;
-    }
+}
 }
